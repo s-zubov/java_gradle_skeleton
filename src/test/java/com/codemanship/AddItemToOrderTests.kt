991384cf870b342aa9b7c9fdabdceb031567bbb0
Sergei Zubov <szubov@protonmail.com>
2026-05-20 18:57:14 +0100

    refactor: extract nested class
    Co-authored-by: gingeriale <yanochkina.mv@gmail.com>

--- src/test/java/com/codemanship/AddItemToOrderTests.kt
+++ src/test/java/com/codemanship/AddItemToOrderTests.kt
@@ -2,20 +2,24 @@ package com.codemanship
 
 import com.codemanship.Product
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class AddItemToOrderTests {
-    @Test
-    fun `add an item when sufficient stock is available puts a temporary hold`() {
-        val product = Product()
-        Assertions.assertEquals(1, product.hold)
-    }
+    @Nested
+    inner class `add an item when sufficient stock is available` {
+        @Test
+        fun `puts a temporary hold`() {
+            val product = Product()
+            Assertions.assertEquals(1, product.hold)
+        }
 
-    @Test
-    fun `add an item when sufficient stock is available sets order quantity 1`() {
-        val order = Order()
-        val item = Item(327, 1)
-        order.addItem(item)
-        Assertions.assertEquals(1, order.quantityOf(327))
+        @Test
+        fun `sets order quantity to 1`() {
+            val order = Order()
+            val item = Item(327, 1)
+            order.addItem(item)
+            Assertions.assertEquals(1, order.quantityOf(327))
+        }
     }
 }
