926e72b85340aefbaa7f826969cc4d2423398d6b
Sergei Zubov <szubov@protonmail.com>
2026-05-20 19:24:55 +0100

    refactor: extract add an item when sufficient stock is available arrangement
    Co-authored-by: gingeriale <yanochkina.mv@gmail.com>

--- src/test/java/com/codemanship/AddItemToOrderTests.kt
+++ src/test/java/com/codemanship/AddItemToOrderTests.kt
@@ -1,17 +1,18 @@
 package com.codemanship
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class AddItemToOrderTests {
     @Nested
     inner class `add an item when sufficient stock is available` {
+        val order = Order()
+        val product = Product(327, stockQty = 7)
+
         @Test
         fun `puts a temporary hold`() {
-            val order = Order()
-            val product = Product(327, stockQty = 7)
-
             order.add(product, 1)
 
             Assertions.assertEquals(1, product.holdQty)
@@ -19,9 +20,6 @@ class AddItemToOrderTests {
 
         @Test
         fun `sets order quantity to 1`() {
-            val order = Order()
-            val product = Product(327, stockQty = 7)
-
             order.add(product, 1)
 
             Assertions.assertEquals(1, order.quantityOf(327))
