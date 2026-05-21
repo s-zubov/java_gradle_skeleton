ec5c5835ec4a7ae77397dbb0135cd3c0970ec1e6
Sergei Zubov <szubov@protonmail.com>
2026-05-21 22:00:00 +0100

    refactor: replace items list with map
    Co-authored-by: gingeriale <yanochkina.mv@gmail.com>

--- src/main/java/com/codemanship/Item.kt
+++
@@ -1,6 +0,0 @@
-package com.codemanship
-
-data class Item(
-    val id: Int,
-    val quantity: Int
-)
--- src/main/java/com/codemanship/Order.kt
+++ src/main/java/com/codemanship/Order.kt
@@ -1,28 +1,27 @@
 package com.codemanship
 
 class Order() {
-    private val items: MutableList<Item> = mutableListOf()
+    private val items: MutableMap<Int, Int> = mutableMapOf()
 
     constructor(product: Product, quantity: Int) : this() {
-        val item = Item(product.id, quantity)
-        items.add(item)
+        items[product.id] = quantity
     }
 
-    fun quantityOf(id: Int): Int = items.singleOrNull { it.id == id }?.quantity ?: 0
+    fun quantityOf(id: Int): Int = items[id] ?: 0
 
     fun add(product: Product, quantity: Int) {
         if (product.stockQty < product.holdQty + quantity) {
             throw InsufficientStockException(product)
         }
 
-        val item = Item(product.id, quantity)
-        items.add(item)
+        items[product.id] = (items[product.id] ?: 0) + quantity
+
         product.hold(quantity)
     }
 
     fun remove(product: Product) {
-        val itemToRemove = items.single { it.id == product.id }
-        items.remove(itemToRemove)
-        product.releaseHold(itemToRemove.quantity)
+        val itemCountToRemove = items[product.id] ?: 0
+        items.remove(product.id)
+        product.releaseHold(itemCountToRemove)
     }
 }
