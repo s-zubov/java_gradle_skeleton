35e77a3f8323a2df677329b06b3a7f6416149163
Sergei Zubov <szubov@protonmail.com>
2026-05-25 19:20:13 +0100

    refactor: remove default holdQty
    Co-authored-by: gingeriale <yanochkina.mv@gmail.com>

--- src/main/java/com/codemanship/Product.kt
+++ src/main/java/com/codemanship/Product.kt
@@ -3,7 +3,7 @@ package com.codemanship
 import java.math.BigDecimal
 
 class Product {
-    constructor(id: Int, price: BigDecimal, stockQty: Int, holdQty: Int = 0) {
+    constructor(id: Int, price: BigDecimal, stockQty: Int, holdQty: Int) {
         this.id = id
         this.holdQty = holdQty
         this.stockQty = stockQty
--- src/test/java/com/codemanship/AddItemToOrderTests.kt
+++ src/test/java/com/codemanship/AddItemToOrderTests.kt
@@ -12,7 +12,7 @@ class AddItemToOrderTests {
         @Test
         fun `puts a temporary hold`() {
             val order = Order()
-            val product = Product(327, price = BigDecimal("159.99"), stockQty = 7)
+            val product = Product(327, price = BigDecimal("159.99"), stockQty = 7, holdQty = 0)
 
             order.add(product, 1)
 
@@ -22,7 +22,7 @@ class AddItemToOrderTests {
         @Test
         fun `sets order quantity to 1`() {
             val order = Order()
-            val product = Product(327, price = BigDecimal("159.99"), stockQty = 7)
+            val product = Product(327, price = BigDecimal("159.99"), stockQty = 7, holdQty = 0)
 
             order.add(product, 1)
 
@@ -35,7 +35,7 @@ class AddItemToOrderTests {
         @Test
         fun `order contains no items`() {
             val order = Order()
-            val product = Product(id = 327, price = BigDecimal("159.99"), stockQty = 1)
+            val product = Product(id = 327, price = BigDecimal("159.99"), stockQty = 1, holdQty = 0)
 
             runCatching { order.add(product, 2) }
 
@@ -45,7 +45,7 @@ class AddItemToOrderTests {
         @Test
         fun `insufficient stock error is raised`() {
             val order = Order()
-            val product = Product(id = 327, price = BigDecimal("159.99"), stockQty = 1)
+            val product = Product(id = 327, price = BigDecimal("159.99"), stockQty = 1, holdQty = 0)
 
             try {
                 order.add(product, 2)
