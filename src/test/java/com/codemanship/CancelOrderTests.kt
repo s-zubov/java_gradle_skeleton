e4e04c9e77f44f59e4786ca181cb7c3b095e4191
Sergei Zubov <szubov@protonmail.com>
2026-06-04 20:09:00 +0100

    test: cancel order doesnt change stock
    Co-authored-by: gingeriale <yanochkina.mv@gmail.com>

--- src/test/java/com/codemanship/CancelOrderTests.kt
+++ src/test/java/com/codemanship/CancelOrderTests.kt
@@ -20,4 +20,20 @@ class CancelOrderTests {
         Assertions.assertEquals(0, ibanezProduct.holdQty)
         Assertions.assertEquals(0, fenderProduct.holdQty)
     }
+
+    @Test
+    fun `doesn't change stock`() {
+        val ibanezProduct = Product(
+            id = 327, description = "Ibanez Tube Screamer", price = BigDecimal("159.99"), stockQty = 7, holdQty = 2
+        )
+        val fenderProduct = Product(
+            id = 811, description = "Fender Deluxe Reverb", price = BigDecimal("1799.00"), stockQty = 2, holdQty = 1
+        )
+        val order = Order(listOf(ibanezProduct to 2, fenderProduct to 1))
+
+        order.cancel()
+
+        Assertions.assertEquals(7, ibanezProduct.stockQty)
+        Assertions.assertEquals(2, fenderProduct.stockQty)
+    }
 }
