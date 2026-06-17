bfd42c89ab5edf70322f71d8302aedae97fd571f
Margarita Ianochkina <yanochkina.mv@gmail.com>
2026-06-17 16:10:36 +0100

    refactor: database constructor parameter

--- src/main/java/com/codemanship/infra/MongoOrders.kt
+++ src/main/java/com/codemanship/infra/MongoOrders.kt
@@ -2,15 +2,11 @@ package com.codemanship.infra
 
 import com.codemanship.application.Orders
 import com.codemanship.domain.Order
-import com.mongodb.kotlin.client.MongoCollection
 import com.mongodb.kotlin.client.MongoDatabase
 import org.bson.types.ObjectId
 
-class MongoOrders(val database: MongoDatabase) : Orders {
-    private val collection: MongoCollection<OrderData>
-        get() {
-            return database.getCollection<OrderData>("orders")
-        }
+class MongoOrders(database: MongoDatabase) : Orders {
+    private val collection = database.getCollection<OrderData>("orders")
 
     override fun save(order: Order): Order {
         val data = OrderData(
--- src/main/java/com/codemanship/infra/MongoProducts.kt
+++ src/main/java/com/codemanship/infra/MongoProducts.kt
@@ -3,15 +3,11 @@ package com.codemanship.infra
 import com.codemanship.application.Products
 import com.codemanship.domain.Product
 import com.mongodb.client.model.Filters.eq
-import com.mongodb.kotlin.client.MongoCollection
 import com.mongodb.kotlin.client.MongoDatabase
 import org.bson.types.ObjectId
 
-class MongoProducts(val database: MongoDatabase) : Products {
-    private val collection: MongoCollection<ProductData>
-        get() {
-            return database.getCollection<ProductData>("products")
-        }
+class MongoProducts(database: MongoDatabase) : Products {
+    private val collection = database.getCollection<ProductData>("products")
 
     override fun get(id: String): Product {
         val objectId = ObjectId(id)
